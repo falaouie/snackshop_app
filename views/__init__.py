0ce3cab16c028d71ae8f293cddb714aedda9df16
Fadi Alaouie <falaouie@yahoo.com>
2025-02-18 08:08:12 +0200

    modified init files

---
+++ views/__init__.py
@@ -0,0 +1,8 @@
+# views/__init__.py
+from .main_window import MainWindow
+from .pos_view import POSView
+
+__all__ = [
+    'MainWindow',
+    'POSView',
+]
--- views/auth/__init__.py
+++ views/auth/__init__.py
@@ -1,5 +1,10 @@
+# views/auth/__init__.py
 from .auth_container import AuthenticationContainer
 from .pin_view import PinView
 from .user_id_view import UserIDView
 
-__all__ = ['AuthenticationContainer', 'PinView', 'UserIDView']
+__all__ = [
+    'AuthenticationContainer',
+    'PinView',
+    'UserIDView'
+]
